7f45fdfc3a02e982246de766d5e473a6911bb90e
Ahmed Khalf <ahmedkhalf567@gmail.com>
2021-04-17 21:01:45 +0400

    Add application struct and input handling

--- main.go
+++ main.go
@@ -4,32 +4,35 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strings"
-	"time"
+	"sync"
+	"unicode/utf8"
 
 	"github.com/beevik/terminfo"
+	"github.com/containerd/console"
 )
 
-type TermInfoNotInDir struct{
-	TermInfoName string
+type TermInfoNotInDir struct {
+	TermInfoName    string
 	SearchDirectory string
 }
 
 func (m *TermInfoNotInDir) Error() string {
-    return fmt.Sprintf("Could not find terminfo %q in %q", m.TermInfoName, m.SearchDirectory)
+	return fmt.Sprintf("Could not find terminfo %q in %q", m.TermInfoName, m.SearchDirectory)
 }
 
-type TermInfoNotFound struct{
-	TermInfoName string
+type TermInfoNotFound struct {
+	TermInfoName      string
 	SearchDirectories string
 }
 
 func (m *TermInfoNotFound) Error() string {
-    return fmt.Sprintf("Could not find terminfo %q in any of %q", m.TermInfoName, m.SearchDirectories)
+	return fmt.Sprintf("Could not find terminfo %q in any of %q", m.TermInfoName, m.SearchDirectories)
 }
 
 func LoadFromFile(file string) (*terminfo.TermInfo, error) {
@@ -58,7 +61,7 @@ func LoadFromDir(dir string, name string) (*terminfo.TermInfo, error) {
 	}
 
 	return nil, &TermInfoNotInDir{
-		TermInfoName: name,
+		TermInfoName:    name,
 		SearchDirectory: dir,
 	}
 }
@@ -98,7 +101,7 @@ func LoadFromName(name string) (*terminfo.TermInfo, error) {
 	}
 
 	return nil, &TermInfoNotFound{
-		TermInfoName: name,
+		TermInfoName:      name,
 		SearchDirectories: strings.Join(searchDirs, ", "),
 	}
 }
@@ -107,21 +110,93 @@ func LoadFromEnv() (*terminfo.TermInfo, error) {
 	return LoadFromName(os.Getenv("TERM"))
 }
 
-func main() {
+type Application struct {
+	ti     *terminfo.TermInfo
+
+	mu     *sync.Mutex
+
+	input  io.Reader
+	output io.Writer
+}
+
+func (a *Application) EnterFullScreen() {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if smcup, ok := a.ti.GetStringCap("smcup"); ok {
+		fmt.Print(smcup)
+	}
+}
+
+func (a *Application) ExitFullScreen() {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if rmcup, ok := a.ti.GetStringCap("rmcup"); ok {
+		fmt.Print(rmcup)
+	}
+}
+
+func (a *Application) Start() {
+	// First we set enable cbreak and noecho, this helps us get user input
+	// as the user types and doesn't show the input to screen.
+	current := console.Current()
+	defer current.Reset()
+	current.SetRaw()
+	current.DisableEcho()
+
+	for {
+		r := readInput(a.input)
+		if r == 'q' {
+			break
+		}
+	}
+}
+
+func NewApplication() *Application {
 	ti, err := LoadFromEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	smcup, ok := ti.GetStringCap("smcup") // Enter FullScreen
-	if ok {
-		fmt.Print(smcup)
+	a := &Application{
+		ti:     ti,
+		mu:    &sync.Mutex{},
+		input:  os.Stdin,
+		output: os.Stdout,
 	}
 
-	time.Sleep(2 * time.Second)
+	return a
+}
 
-	rmcup, ok := ti.GetStringCap("rmcup") // Exit FullScreen
-	if ok {
-		fmt.Print(rmcup)
+func readInput(input io.Reader) rune {
+	var buf [256]byte
+	numBytes, _ := input.Read(buf[:])
+
+	var runes []rune
+	b := buf[:numBytes]
+
+	// Translate input into runes. In most cases we'll receive exactly one
+	// rune, but there are cases, particularly when an input method editor is
+	// used, where we can receive multiple runes at once.
+	for i, w := 0, 0; i < len(b); i += w {
+		r, width := utf8.DecodeRune(b[i:])
+		if r == utf8.RuneError {
+			fmt.Println("Could not decode rune")
+		}
+		runes = append(runes, r)
+		w = width
 	}
+
+	if len(runes) == 1 {
+		return runes[0]
+	}
+	return ' '
+}
+
+func main() {
+	app := NewApplication()
+	app.EnterFullScreen()
+	app.Start()
+	app.ExitFullScreen()
 }
