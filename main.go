a4bff2fad7bc32e321af97c4d784b83508fc9176
Ahmed Khalf <ahmedkhalf567@gmail.com>
2021-04-18 15:01:56 +0400

    Replace beevik/terminfo with xo/terminfo

--- main.go
+++ main.go
@@ -1,117 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"os/user"
-	"path/filepath"
-	"strings"
 	"sync"
 	"unicode/utf8"
 
-	"github.com/beevik/terminfo"
+	"github.com/xo/terminfo"
 	"github.com/containerd/console"
 )
 
-type TermInfoNotInDir struct {
-	TermInfoName    string
-	SearchDirectory string
-}
-
-func (m *TermInfoNotInDir) Error() string {
-	return fmt.Sprintf("Could not find terminfo %q in %q", m.TermInfoName, m.SearchDirectory)
-}
-
-type TermInfoNotFound struct {
-	TermInfoName      string
-	SearchDirectories string
-}
-
-func (m *TermInfoNotFound) Error() string {
-	return fmt.Sprintf("Could not find terminfo %q in any of %q", m.TermInfoName, m.SearchDirectories)
-}
-
-func LoadFromFile(file string) (*terminfo.TermInfo, error) {
-	ansifile, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ansifile.Close()
-
-	ansireader := bufio.NewReader(ansifile)
-
-	ti, err := terminfo.Read(ansireader)
-	return ti, err
-}
-
-func LoadFromDir(dir string, name string) (*terminfo.TermInfo, error) {
-	rootFile := filepath.Join(dir, name)
-	nestedFile := filepath.Join(dir, string(name[0]), name)
-
-	if _, err := os.Stat(rootFile); err == nil {
-		return LoadFromFile(rootFile)
-	} else if os.IsNotExist(err) {
-		if _, err := os.Stat(nestedFile); err == nil {
-			return LoadFromFile(nestedFile)
-		}
-	}
-
-	return nil, &TermInfoNotInDir{
-		TermInfoName:    name,
-		SearchDirectory: dir,
-	}
-}
-
-func LoadFromName(name string) (*terminfo.TermInfo, error) {
-	var searchDirs []string
-
-	// $TERMINFO
-	if dir := os.Getenv("TERMINFO"); dir != "" {
-		searchDirs = append(searchDirs, dir)
-	}
-
-	// $HOME/.terminfo
-	u, err := user.Current()
-	if err != nil {
-		return nil, err
-	}
-	searchDirs = append(searchDirs, filepath.Join(u.HomeDir, ".terminfo"))
-
-	// $TERMINFO_DIRS
-	if dirs := os.Getenv("TERMINFO_DIRS"); dirs != "" {
-		searchDirs = append(searchDirs, strings.Split(dirs, ":")...)
-	}
-
-	searchDirs = append(searchDirs, "/etc/terminfo", "/lib/terminfo", "/usr/share/terminfo")
-
-	var notindir *TermInfoNotInDir
-	for _, dir := range searchDirs {
-		ti, err := LoadFromDir(dir, name)
-		if err != nil {
-			if ok := errors.As(err, &notindir); !ok {
-				return ti, err
-			}
-		} else {
-			return ti, err
-		}
-	}
-
-	return nil, &TermInfoNotFound{
-		TermInfoName:      name,
-		SearchDirectories: strings.Join(searchDirs, ", "),
-	}
-}
-
-func LoadFromEnv() (*terminfo.TermInfo, error) {
-	return LoadFromName(os.Getenv("TERM"))
-}
-
 type Application struct {
-	ti     *terminfo.TermInfo
+	ti     *terminfo.Terminfo
 
 	mu     *sync.Mutex
 
@@ -123,18 +25,14 @@ func (a *Application) EnterFullScreen() {
 	a.mu.Lock()
 	defer a.mu.Unlock()
 
-	if smcup, ok := a.ti.GetStringCap("smcup"); ok {
-		fmt.Print(smcup)
-	}
+	a.ti.Fprintf(a.output, terminfo.EnterCaMode)
 }
 
 func (a *Application) ExitFullScreen() {
 	a.mu.Lock()
 	defer a.mu.Unlock()
 
-	if rmcup, ok := a.ti.GetStringCap("rmcup"); ok {
-		fmt.Print(rmcup)
-	}
+	a.ti.Fprintf(a.output, terminfo.ExitCaMode)
 }
 
 func (a *Application) Start() {
@@ -154,7 +52,7 @@ func (a *Application) Start() {
 }
 
 func NewApplication() *Application {
-	ti, err := LoadFromEnv()
+	ti, err := terminfo.LoadFromEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -196,7 +94,9 @@ func readInput(input io.Reader) rune {
 
 func main() {
 	app := NewApplication()
+
 	app.EnterFullScreen()
+	defer app.ExitFullScreen()
+
 	app.Start()
-	app.ExitFullScreen()
 }
